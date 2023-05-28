84d56f5d6d3266e64fccbdd1e163274373185d8b
Brath <brathbarbozaagustin@gmail.com>
2023-05-27 23:54:30 -0500

    test dinamic route

--- src/pages/new/[id].js
+++ src/pages/new/[id].js
@@ -1,25 +1,21 @@
 import { useRouter } from "next/router";
-import dataJson from '../data/data.json'
+import dataJson from "../../data/data.json";
 
-export default function Semana() {
+export default function New() {
   const id = useRouter().query.id;
-  const data2 = dataJson.semanas
+  const data2 = dataJson.semanas;
 
   return (
     <>
       <section className="p-52 bg-slate-500">
         <div>{id}</div>
-        {data2[id - 1] 
-        ? 
-        (
+        {data2[id - 1] ? (
           <>
             <div>{data2[id - 1].id}</div>
             <p>{data2[id - 1].titulo}</p>
             <p>{data2[id - 1].contenido}</p>
           </>
-        ) 
-        : 
-        (
+        ) : (
           <p>no hay</p>
         )}
       </section>
