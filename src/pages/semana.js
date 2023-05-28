894d31f0a482123858ac5b9b496ff6acd5289185
Brath <brathbarbozaagustin@gmail.com>
2023-05-27 23:36:44 -0500

    test api v3

--- src/pages/semana.js
+++ src/pages/semana.js
@@ -1,16 +1,23 @@
 import { useRouter } from "next/router";
 import { useState, useEffect } from "react";
+import dataJson from '../data/data.json'
 
 export default function Semana() {
   const id = useRouter().query.id;
 
   const [data, setData] = useState([]);
+  /* const [data2, setData2] = useState([])
+
+  setData2(dataJson.semanas) */
+  const data2 = dataJson.semanas
 
   const fetchData = async () => {
     try {
-      const response = await fetch("/api/data");
+      const response = await fetch("./api/data");
       const jsonData = await response.json();
+      /* console.log(dataJson.semanas); */
       setData(jsonData.semanas);
+      /* console.log(dataJson) */
     } catch (error) {
       console.error("Error fetching data:", error);
     }
@@ -29,6 +36,16 @@ export default function Semana() {
             <div>{data[id - 1].id}</div>
             <p>{data[id - 1].titulo}</p>
             <p>{data[id - 1].contenido}</p>
+          </>
+        )}
+        <br />
+        <hr />
+        <br />
+        {data2[id - 1] && (
+          <>
+            <div>{data2[id - 1].id}</div>
+            <p>{data2[id - 1].titulo}</p>
+            <p>{data2[id - 1].contenido}</p>
           </>
         )}
       </section>
