cc4ab60c5201946f63687617ce2a44076d54d1d2
Brath <brathbarbozaagustin@gmail.com>
2023-05-30 22:54:51 -0500

    footer link left

--- src/components/footerlink/index.jsx
+++ src/components/footerlink/index.jsx
@@ -11,7 +11,7 @@ const FooterLink = ({link, name, color}) => {
   return (
     <Link
       href={link}
-      className={`${styles.footer_line} relative w-fit after:content-[''] after:absolute after:w-full after:h-1 after:-bottom-2 after:-left-1 ${coloresafter[color]} `}
+      className={`${styles.footer_line} relative w-fit after:content-[''] after:absolute after:w-full after:h-1 after:-bottom-2 after:-left-0 ${coloresafter[color]} `}
       scroll={false}
     >
       {name}
